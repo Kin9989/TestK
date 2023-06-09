922a01ad158ff240c72d2d0669d8d4486bd9c1b4
Kin9989 <HaoQH@int.t4tek.co>
2023-06-10 01:06:25 +0700

    đâ

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-// import MessengerChat from './MessengerChat';
 import ReactMessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   return (
     <div className="App">
       Hello My App <br/>
-      Test Chat messger<br/>
+      Test Chat Messenger<br/>
       Kinranx
-      hello what your name
-      <MessengerCustomerChat
-      pageId="113817248403137"
-      appId="954564055758448"
-      
-  />,
+      hello what is your name
+      <ReactMessengerCustomerChat
+        pageId="113817248403137"
+        appId="954564055758448"
+      />
     </div>
   );
 }
