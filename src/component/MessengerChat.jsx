8e644770bb270fab061722ea135711eedcea86bf
Kin9989 <HaoQH@int.t4tek.co>
2023-06-09 16:28:27 +0700

    daada

--- src/component/MessengerChat.jsx
+++ src/component/MessengerChat.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { FacebookProvider, MessengerCustomerChat } from 'react-facebook';
 
 const MessengerChat = () => {
   useEffect(() => {
@@ -21,14 +20,40 @@ const MessengerChat = () => {
   }, []);
 
   return (
-    <FacebookProvider appId="100093455920327">
-      <MessengerCustomerChat
-        pageId="113817248403137"
-        appId="100093455920327"
-        htmlRef=""
-      />
-    </FacebookProvider>
+    <div>
+      <div id="fb-root"></div>
+      <div id="fb-customer-chat" className="fb-customerchat"></div>
+
+      <script dangerouslySetInnerHTML={{
+        __html: `
+          var chatbox = document.getElementById('fb-customer-chat');
+          chatbox.setAttribute("page_id", "113817248403137");
+          chatbox.setAttribute("attribution", "install_email");
+          chatbox.setAttribute("attribution_version", "biz_inbox");
+        `
+      }}></script>
+
+      <script dangerouslySetInnerHTML={{
+        __html: `
+          window.fbAsyncInit = function() {
+            FB.init({
+              xfbml: true,
+              version: 'v17.0'
+            });
+          };
+          
+          (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
+        `
+      }}></script>
+    </div>
   );
 };
 
 export default MessengerChat;
+
