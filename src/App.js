88f2ac4b39092721285dd4c04608f7603f613c1d
Kin9989 <HaoQH@int.t4tek.co>
2023-06-09 16:53:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import MessengerChat from './component/MessengerChat';
+
 
 function App() {
   return (
     <div className="App">
       Haoooo
-      <MessengerChat />
+     
     </div>
   );
 }
