080f2e0bd294e581c5aaaf867c8ce3440566ac30
Kin9989 <HaoQH@int.t4tek.co>
2023-06-10 01:04:45 +0700

    đâ

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import MessengerChat from './MessengerChat';
-import MessengerCustomerChat from 'react-messenger-customer-chat ';
+import ReactMessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   return (
